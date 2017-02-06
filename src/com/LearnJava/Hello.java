ade827492225d7ab7ff7fb6457b31c716f86e67e
poorhack <wangyf2007@163.com>
2017-02-06 11:13:23 +0800

    add Hello world
    add hello world

---
+++ src/com/LearnJava/Hello.java
@@ -0,0 +1,16 @@
+package com.LearnJava;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=0;
+		do
+		{
+			System.out.println("Hello World\r\n");
+			i++;
+		}while(i<5);
+		
+	}
+
+}
